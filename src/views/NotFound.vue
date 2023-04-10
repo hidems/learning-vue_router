936a4980f628f39de0f79257d5d75ef8c8da10ec
hidems <miura.eleven@gmail.com>
2023-04-10 13:19:09 +0200

    404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <button @click="goToHome">Go Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  methods: {
+    goToHome() {
+      this.$router.push({ name: 'home' });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
